6ee7de4fd2171997a1d27fd69ffabb2857eeeff6
jbsselim <selim.jean-baptiste-simonne@epitech.eu>
2022-05-05 13:58:56 +0200

    few changes

---
+++ precache-manifest.0e8c4e7fd99a1d43607731b7f60c37e9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c584ce7c8c282ab7c9320bf797425b6e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "90c9f2462a8758556176",
+    "url": "/static/css/main.59851a23.chunk.css"
+  },
+  {
+    "revision": "b427efcd0d65caba4a65",
+    "url": "/static/js/2.b51f9938.chunk.js"
+  },
+  {
+    "revision": "cd94619111e2ab78ce54eac27b302772",
+    "url": "/static/js/2.b51f9938.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90c9f2462a8758556176",
+    "url": "/static/js/main.0ce06fde.chunk.js"
+  },
+  {
+    "revision": "b8c7440aed72a32d7633",
+    "url": "/static/js/runtime-main.3fbc67ef.js"
+  },
+  {
+    "revision": "4b9dc077a5e83bc17478662d0144dc92",
+    "url": "/static/media/memories-Logo.4b9dc077.png"
+  },
+  {
+    "revision": "18ebdb6d48b70ab2e72ac3bf9154d8be",
+    "url": "/static/media/memories-Text.18ebdb6d.png"
+  }
+]);
